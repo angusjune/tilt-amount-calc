72859efd5db6843107bc91e4eb6600dc0b4fba4d
Angus Zhu <angus.is.online@gmail.com>
2024-10-18 15:13:16 +0800

    First update

---
+++ app/components/dragInput.tsx
@@ -0,0 +1,145 @@
+import NumberFlow from "@number-flow/react";
+import { AnimatePresence, motion, PanInfo } from "framer-motion";
+import { useCallback, useEffect, useRef, useState } from "react";
+import useMeasure from "react-use-measure";
+
+interface DragInputProps {
+    label?: string;
+    min?: number;
+    step?: number;
+    value?: number;
+    unit: string;
+    onInput?: (value: number) => void;
+}
+
+export default function DragInput({
+    label,
+    min = 0,
+    step = 1,
+    value,
+    unit,
+    onInput,
+}: DragInputProps) {
+    const constraintsRef = useRef(null);
+    const [ref, bounds] = useMeasure();
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [preVal, setPrevVal] = useState(value);
+    const [val, setVal] = useState(value);
+    const [increment, setIncrement] = useState(0);
+    const [dragging, setDragging] = useState(false);
+    const [ruleScaleX, setRuleScaleX] = useState(0);
+
+    const handleDrag = (event: Event, info: PanInfo) => {
+        const lengthPerStep = Math.max(12, bounds.width / 30);
+        const { x } = info.offset;
+        const increment = Math.floor(x / lengthPerStep) * step;
+        setIncrement(increment);
+
+        const ruleScaleX = x / (bounds.width / 2);
+        setRuleScaleX(ruleScaleX);
+    };
+
+    const handleDragEnd = useCallback(() => {
+        setDragging(false);
+        setPrevVal(val);
+        setRuleScaleX(0);
+    }, [val]);
+
+    useEffect(() => {
+        if (dragging && !isNaN(preVal!)) {
+            setVal(Math.max(min, preVal! + increment));
+        }
+    }, [preVal, increment, dragging]);
+
+    useEffect(() => {
+        if (!isNaN(value!) && !isLoaded) {
+            setVal(value);
+            setPrevVal(value);
+            setIsLoaded(true);
+        }
+    }, [value, isLoaded]);
+
+    useEffect(() => {
+        if (onInput && !isNaN(val!)) {
+            onInput(val!);
+        }
+    }, [val]);
+
+    return (
+        <div className="flex flex-col w-full gap-2">
+            <motion.div layout className="flex text-3xl items-center">
+                <motion.div
+                    layout
+                    className="font-[family-name:var(--font-geist-sans)] opacity-60"
+                    animate={{ opacity: dragging ? 0.1 : 0.6 }}
+                >
+                    {label}
+                </motion.div>
+
+                <motion.div
+                    layout
+                    className="flex-1 text-right font-[family-name:var(--font-geist-mono)]"
+                >
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: isNaN(val!) ? 0 : 1 }}
+                    >
+                        <NumberFlow
+                            value={val ?? 0}
+                            locales="en-US"
+                            animated={false}
+                            format={{
+                                style: "unit",
+                                unit,
+                                unitDisplay: "narrow",
+                                minimumFractionDigits:
+                                    String(step).split(".")[1]?.length || 0,
+                            }}
+                        ></NumberFlow>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+
+            {/* Track */}
+            <motion.div
+                ref={ref}
+                className="relative h-20 w-full bg-[#212121] rounded-xl"
+            >
+                <motion.div ref={constraintsRef} className="absolute inset-0">
+                    <AnimatePresence>
+                        {ruleScaleX !== 0 && (
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                            >
+                                <motion.div className="absolute left-[calc(50%-6px)] right-0 top-[calc(50%-6px)] w-[12px] aspect-square rounded-full bg-[#323232]"></motion.div>
+                                <motion.div
+                                    className="absolute left-[50%] right-0 top-[calc(50%-1px)] h-[2px] bg-[#323232] origin-left"
+                                    initial={{ scaleX: 0 }}
+                                    animate={{
+                                        scaleX: ruleScaleX,
+                                    }}
+                                    transition={{ duration: 0.1 }}
+                                ></motion.div>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                    {/* Knob */}
+                    <motion.div
+                        className="absolute top-2 bottom-2 w-12 rounded-md custom-box left-[calc(50%-1.5rem)] touch-manipulation"
+                        drag={!isNaN(val!) ? "x" : false}
+                        whileDrag={{ filter: "brightness(1.5)" }}
+                        dragConstraints={constraintsRef}
+                        dragSnapToOrigin={true}
+                        dragElastic={0.2}
+                        onDragStart={() => setDragging(true)}
+                        onDrag={handleDrag}
+                        onDragEnd={handleDragEnd}
+                    ></motion.div>
+                </motion.div>
+            </motion.div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,101 @@
-import Image from "next/image";
+"use client";
+
+import { motion } from "framer-motion";
+import NumberFlow from "@number-flow/react";
+import { useCallback, useEffect, useState } from "react";
+import DragInput from "./components/dragInput";
+
+function calculateTiltAngle(
+    focalLengthMm: number,
+    focusPlaneDistanceM: number
+): number {
+    const h = focusPlaneDistanceM * 1000; // Convert meters to millimeters
+    const thetaRadians = Math.atan(focalLengthMm / h);
+    const thetaDegrees = thetaRadians * (180 / Math.PI);
+    return thetaDegrees;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [tiltAngle, setTiltAngle] = useState(0);
+    const [focalLengthMm, setFocalLengthMm] = useState<number | undefined>(
+        undefined
+    );
+    const [focusPlaneDistanceM, setFocusPlaneDistanceM] = useState<
+        number | undefined
+    >(undefined);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    useEffect(() => {
+        const storedFocalLengthMm = localStorage.getItem("focalLengthMm") ?? 16;
+        const storedFocusPlaneDistanceM =
+            localStorage.getItem("focusPlaneDistanceM") ?? 0.5;
+
+        setFocalLengthMm(Number(storedFocalLengthMm));
+        setFocusPlaneDistanceM(Number(storedFocusPlaneDistanceM));
+
+        setIsLoaded(true);
+    }, []);
+
+    useEffect(() => {
+        getTitleAngle();
+    }, [focalLengthMm, focusPlaneDistanceM]);
+
+    useEffect(() => {
+        if (isLoaded && !isNaN(focalLengthMm!)) {
+            localStorage.setItem("focalLengthMm", focalLengthMm!.toString());
+        }
+    }, [isLoaded, focalLengthMm]);
+
+    useEffect(() => {
+        if (isLoaded && !isNaN(focusPlaneDistanceM!)) {
+            localStorage.setItem(
+                "focusPlaneDistanceM",
+                focusPlaneDistanceM!.toString()
+            );
+        }
+    }, [isLoaded, focusPlaneDistanceM]);
+
+    const getTitleAngle = useCallback(() => {
+        if (!isNaN(focalLengthMm!) && !isNaN(focusPlaneDistanceM!)) {
+            const angle = calculateTiltAngle(
+                focalLengthMm!,
+                focusPlaneDistanceM!
+            );
+            setTiltAngle(isNaN(angle) ? 0 : angle);
+        }
+    }, [focalLengthMm, focusPlaneDistanceM]);
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <motion.div className="font-[family-name:var(--font-geist-sans)] font-semibold gradient-text text-8xl inline-flex mr-[-0.5ch]">
+                    <NumberFlow
+                        value={tiltAngle}
+                        format={{ maximumFractionDigits: 1 }}
+                        style={{ fontVariantNumeric: "tabular-nums" }}
+                    />
+                    <motion.span layout>°</motion.span>
+                </motion.div>
+
+                <div className="w-full flex gap-8 items-center flex-col sm:flex-row">
+                    <DragInput
+                        label="Focal Length"
+                        value={focalLengthMm}
+                        onInput={(val) => setFocalLengthMm(val)}
+                        min={1}
+                        unit="millimeter"
+                    />
+
+                    <DragInput
+                        label="Camera to Ground"
+                        value={focusPlaneDistanceM}
+                        step={0.1}
+                        min={0.1}
+                        onInput={(val) => setFocusPlaneDistanceM(val)}
+                        unit="meter"
+                    />
+                </div>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
